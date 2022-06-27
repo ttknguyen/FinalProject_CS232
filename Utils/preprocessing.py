6c51f405ef81885f14435df2740857dd168382f5
Thai Tran Khanh Nguyen <nguyen.thai@huuk.social>
2022-06-27 10:19:55 +0700

    Update api

--- Utils/preprocessing.py
+++ Utils/preprocessing.py
@@ -1,53 +1,34 @@
 import tensorflow as tf
+from tensorflow import keras
+import librosa
+import numpy as np
 
-def encode_single_sample(wav_file, label, char_to_num, 
-                            lower_edge_hertz = 80.0, upper_edge_hertz = 7600.0, num_mel_bins = 80, 
-                            frame_length = 256, frame_step = 160, fft_length = 384):
-    """
-    Describes the transformation that we apply to each element of our dataset
-    """
-    ###########################################
-    ##  Process the Audio
-    ##########################################
-    # 1. Read wav file
-    file = tf.io.read_file(wav_file)
-    # 2. Decode the wav file
-    audio, sampling_rate = tf.audio.decode_wav(file)
-    audio = tf.squeeze(audio, axis=-1)
-    # 3. Change type to float
+def encode_single_sample(wav_file, frame_length=256, frame_step=160, fft_length=384):
+    # Downsampling the file
+    audio, _ = librosa.load(wav_file, sr=16000)
+    # Change type to float
     audio = tf.cast(audio, tf.float32)
-    # 4. Get the spectrogram
-    stfts = tf.signal.stft(
+    # Get the spectrogram
+    spectrogram = tf.signal.stft(
         audio, frame_length=frame_length, frame_step=frame_step, fft_length=fft_length
     )
-    # 5. We only need the magnitude, which can be derived by applying tf.abs
-    spectrogram = tf.abs(stfts) # get absolute value of complex number
-    spectrogram = tf.math.pow(spectrogram, 2) # get power 
+    # We only need the magnitude, which can be derived by applying tf.abs
+    spectrogram = tf.abs(spectrogram)
+    spectrogram = tf.math.pow(spectrogram, 0.5)
+    # normalisation
+    means = tf.math.reduce_mean(spectrogram, 1, keepdims=True)
+    stddevs = tf.math.reduce_std(spectrogram, 1, keepdims=True)
+    spectrogram = (spectrogram - means) / (stddevs + 1e-10)
 
-    # 6. mel spectrogram
-    num_spectrogram_bins = stfts.shape[-1]
-    linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
-      num_mel_bins, num_spectrogram_bins, sampling_rate, lower_edge_hertz,
-      upper_edge_hertz)
-    mel_spectrograms = tf.tensordot(
-      spectrogram, linear_to_mel_weight_matrix, 1)
-    mel_spectrograms.set_shape(spectrogram.shape[:-1].concatenate(
-      linear_to_mel_weight_matrix.shape[-1:]))
-    # Compute a stabilized log to get log-magnitude mel-scale spectrograms.
-    log_mel_spectrograms = tf.math.log(mel_spectrograms + 1e-6)
+    return spectrogram
 
-    # 8. normalisation
-    means = tf.math.reduce_mean(log_mel_spectrograms, 1, keepdims=True)
-    stddevs = tf.math.reduce_std(log_mel_spectrograms, 1, keepdims=True)
-    log_mel_spectrograms = (log_mel_spectrograms - means) / (stddevs + 1e-10)
-    ###########################################
-    ##  Process the label
-    ##########################################
-    # 9. Convert label to Lower case
-    label = tf.strings.lower(label, encoding="utf-8")
-    # 10. Split the label
-    label = tf.strings.unicode_split(label, input_encoding="UTF-8")
-    # 11. Map the characters in label to numbers
-    label = char_to_num(label)
-    # 12. Return a dict as our model is expecting two inputs
-    return log_mel_spectrograms, label
+def decode_batch_predictions(pred, num_to_char):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
+    # Iterate over the results and get back the text
+    output_text = []
+    for result in results:
+        result = tf.strings.reduce_join(num_to_char(result)).numpy().decode("utf-8")
+        output_text.append(result)
+    return output_text
---
+++ api_speech2text.py
@@ -0,0 +1,51 @@
+from Models.build_model import CTCLoss
+from Utils.preprocessing import encode_single_sample, decode_batch_predictions
+from tensorflow import keras
+from tensorflow.keras import layers
+from jiwer import wer
+from bs4 import BeautifulSoup
+from gtts import gTTS
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import speech_recognition as sr
+
+import string, glob, os, ffmpeg, scipy, scipy.io.wavfile, IPython
+
+
+
+def loadmodel(path):
+    model = tf.keras.models.load_model(path, custom_objects={'CTCLoss': CTCLoss})
+
+def speech2text(model, audio_file, flat = 'our_model'):
+    # The set of characters accepted in the transcription.
+    lowercase_chars = string.ascii_lowercase
+    accented_chars = "àáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹý"
+    final_chars = lowercase_chars + accented_chars  + " "
+    characters = [x for x in final_chars]
+    # Mapping characters to integers
+    char_to_num = keras.layers.StringLookup(vocabulary=characters, oov_token="")
+    # Mapping integers back to original characters
+    num_to_char = keras.layers.StringLookup(
+        vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+    )
+
+    if flat == 'google':
+        # Use library Speech Recognition
+        r = sr.Recognizer()
+        my_audio = sr.AudioFile(audio_file)
+        with my_audio as source:
+            audio = r.record(source)
+        result = r.recognize_google(audio, language="vi-VN")
+
+    else:
+        # Use our trained model
+        spectrogram = np.expand_dims(encode_single_sample(audio_file), axis=0)
+        prediction = model.predict(spectrogram)
+        model_transcription = decode_batch_predictions(prediction, num_to_char)
+        result = model_transcription[0]
+
+    return result
+        
